d5b29d4731aff4dc1e17a54064ae72b72b3525cc
Reem <sreem9504@gmail.com>
2023-05-13 01:23:41 +0300

    MN-11_courseRoutes_1

--- models/courseModel.js
+++ models/courseModel.js
@@ -5,7 +5,7 @@ const courseSchema = new mongoose.Schema({
   description: { type: String, required: false },
   image: { type: String, required: true  },
   class: { type: String, required: true },
-  semester: { type: String, enum:["first", "second", "both"], required: true },
+  semester: { type: String, enum:["الأول", "الثاني", "كليهما"], required: true },
   responsibleUserId: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true }
 });
 
